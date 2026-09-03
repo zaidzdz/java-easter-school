a3ebc63bd52e17186b96dc471f274caa68846d6b
zaidzdz <81050752+zaidzdz@users.noreply.github.com>
2026-09-03 11:57:03 +0000

    format code

--- Easter.java
+++ Easter.java
@@ -1,44 +1,47 @@
-public class Easter{
-    private int[] algorithimValues = new int[14]; 
+public class Easter {
+    private int[] algorithimValues = new int[14];
     private char[] algorithimLetters = new char[14];
     private int algorithimValuesIndex = 0;
     private int year;
+
+    // really just for the print statement at the end
     Easter(int y) {
         this.year = y;
     }
-    
-    public void addAlgorithimValue(char letter, int value){
+
+    public void addAlgorithimValue(char letter, int value) {
         this.algorithimValues[this.algorithimValuesIndex] = value;
         this.algorithimLetters[this.algorithimValuesIndex] = letter;
-        if(this.algorithimValuesIndex != algorithimValues.length-1) { //prevent overflow
-            ++this.algorithimValuesIndex; 
+        if (this.algorithimValuesIndex != algorithimValues.length - 1) { // prevent overflow
+            ++this.algorithimValuesIndex;
         }
     }
-    public int getPreviousAlgorithimValue(){
-        if (this.algorithimValuesIndex <= 0){
+
+    public int getPreviousAlgorithimValue() {
+        if (this.algorithimValuesIndex <= 0) {
             return 0;
         }
-        return this.algorithimValuesIndex-1; 
+        return this.algorithimValuesIndex - 1;
     }
-    public int byLetter(char letter)
-    {
-        //linear search is fine because of small amount of values
-        for(int i = 0; i<this.algorithimLetters.length;i++){
-            //this assumes theres only one value for each letter
-            if(this.algorithimLetters[i] == letter){
+
+    public int byLetter(char letter) {
+        // linear search is fine because of small amount of values
+        for (int i = 0; i < this.algorithimLetters.length; i++) {
+            // this assumes theres only one value for each letter
+            if (this.algorithimLetters[i] == letter) {
                 return this.algorithimValues[i];
             }
         }
-        return '\u0000'; //null char 
+        return '\u0000'; // null char if the letter does not exists i dont know how to throw errors
 
     }
-    public void printAll(){
-        for(int i = 0;i<this.algorithimValues.length; i++){
+
+    public void printAll() {
+        for (int i = 0; i < this.algorithimValues.length; i++) {
             System.out.println(this.algorithimLetters[i] + " = " + this.algorithimValues[i]);
         }
-        System.out.println("Easter in " + this.year + " falls on " + this.byLetter('n') + "/" + (this.byLetter('p')+1) );
+        System.out
+                .println("Easter in " + this.year + " falls on " + this.byLetter('n') + "/" + (this.byLetter('p') + 1));
     }
 
 }
-
-
--- Main.java
+++ Main.java
@@ -1,36 +1,42 @@
-import java.util.Scanner;
 // Zaid Zamani
 // This program will calculate the date easter falls on given a year after 1583.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int y = scanner.nextInt();
+
             Easter easter = new Easter(y);
             easter.addAlgorithimValue('a', y % 19);
             easter.addAlgorithimValue('b', (y / 100));
             easter.addAlgorithimValue('c', y % 100);
+
             easter.addAlgorithimValue('d', easter.byLetter('b') / 4);
             easter.addAlgorithimValue('e', easter.byLetter('b') % 4);
             easter.addAlgorithimValue('f', (easter.byLetter('b') + 8) / 25);
+
             easter.addAlgorithimValue('g', (easter.byLetter('b') - easter.byLetter('f') + 1) / 3);
             easter.addAlgorithimValue('h', (19 * easter.byLetter('a')
                     + easter.byLetter('b')
                     - easter.byLetter('d')
                     - easter.byLetter('g') + 15) % 30);
             easter.addAlgorithimValue('i', easter.byLetter('c') / 4); // i
+
             easter.addAlgorithimValue('k', (easter.byLetter('c') % 4)); // k
-            easter.addAlgorithimValue('r', (32 + 2 * easter.byLetter('e') 
-            + 2 * easter.byLetter('i') 
-            - easter.byLetter('h') 
-            - easter.byLetter('k')) % 7
-            );
-            easter.addAlgorithimValue('m', (easter.byLetter('a') + 11 * easter.byLetter('h') + 22 * easter.byLetter('r')) / 451);
-            int nWithoutDiv = (easter.byLetter('h')+easter.byLetter('r') - 7 * easter.byLetter('m') + 114);
-            easter.addAlgorithimValue('n',  nWithoutDiv / 31);
-            easter.addAlgorithimValue('p',  nWithoutDiv % 31);
+            easter.addAlgorithimValue('r', (32
+                    + 2 * easter.byLetter('e')
+                    + 2 * easter.byLetter('i')
+                    - easter.byLetter('h')
+                    - easter.byLetter('k')) % 7);
+            easter.addAlgorithimValue('m',
+                    (easter.byLetter('a') + 11 * easter.byLetter('h') + 22 * easter.byLetter('r')) / 451);
+
+            int nWithoutDiv = (easter.byLetter('h') + easter.byLetter('r') - 7 * easter.byLetter('m') + 114);
+            easter.addAlgorithimValue('n', nWithoutDiv / 31);
+            easter.addAlgorithimValue('p', nWithoutDiv % 31);
 
             easter.printAll();
-        }   
-        
+        }
+
     }
 }
